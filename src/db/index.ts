f848e3d65495252892b0626373c4da7106486604
onuryilmaaz <onuryilm.41@gmail.com>
2025-12-29 23:20:29 +0300

    ....

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,23 @@
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
+// import * as schema from "./schema/app-schema";
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// export const db = drizzle(pool, { schema });
+
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema/app-schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export const db = drizzle(pool, { schema });
